57573ff06fbaac81b04d2314d90db302a169f267
mactkg <mactkg@gmail.com>
2016-05-22 12:17:50 +0900

    cleaning

--- src/ofApp.h
+++ src/ofApp.h
@@ -7,24 +7,8 @@ class ofApp : public ofBaseApp{
 public:
     void setup();
     void setupGui();
-    void update();
     void draw();
     void drawSub(ofEventArgs & args);
     
-    void keyPressed(int key);
-    void keyReleased(int key);
-    void mouseMoved(int x, int y );
-    void mouseDragged(int x, int y, int button);
-    void mousePressed(int x, int y, int button);
-    void mouseReleased(int x, int y, int button);
-    void mouseEntered(int x, int y);
-    void mouseExited(int x, int y);
-    void windowResized(int w, int h);
-    void dragEvent(ofDragInfo dragInfo);
-    void gotMessage(ofMessage msg);
-    
-    ofParameterGroup parameters;
-    ofParameter<float> radius;
-    ofParameter<ofColor> color;
     ofImage img;
 };
